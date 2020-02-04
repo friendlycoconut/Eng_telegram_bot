ef684f6f8c83600a24c6cb9e6e8805b416ba9d00
friendlycoconut <Travelingflyboy@gmail.com>
2020-02-04 21:40:18 +0200

    123

---
+++ main.py
@@ -0,0 +1,29 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '844758961:AAFhA0ebQKTw2rwRhPOZZjQNfzL26ZWigYw'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.answer("Привет, " + message.from_user.first_name + " \nНапиши мне что-нибудь!")
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.answer("Напиши мне , и я помогу выучить английский!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
