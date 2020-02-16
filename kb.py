25974b75f947e2d26aa697eb999e9b7838beff7a
dexxxed <denis22052000@gmail.com>
2020-02-16 20:56:52 +0200

    and minirefactor

--- kb.py
+++ kb.py
@@ -1,32 +1,21 @@
 from aiogram import types
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from slugify import slugify
 
 
-button_hi = KeyboardButton('Привет! 👋')
-
-greet_kb = ReplyKeyboardMarkup()
-greet_kb.add(button_hi)
-
 keyboard_markup = types.InlineKeyboardMarkup(row_width=3, one_time_keyboard=True)
-# default row_width is 3, so here we can omit it actually
-# kept for clearness
-variant_1_text = 'Я хочу пройти тест'
-variant_1_text_slug = slugify(variant_1_text)
-variant_2_text = 'Я хочу выучить слова'
-variant_2_text_slug = slugify(variant_2_text)
 
-text_and_data = (
-    (variant_1_text, variant_1_text_slug),
-    (variant_2_text, variant_2_text_slug),
-)
-# in real life for the callback_data the callback data factory should be used
-# here the raw string is used for the simplicity
+variant_1_text = 'Я хочу пройти тест'
+variant_2_text = 'Я хочу выучить слова'
+
+text_and_data = [
+    [variant_1_text, slugify(variant_1_text)],
+    [variant_2_text, slugify(variant_1_text)],
+]
+
 row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in text_and_data)
 
 keyboard_markup.row(*row_btns)
 keyboard_markup.add(
-    # url buttons have no callback data
     types.InlineKeyboardButton('Статистика', url='https://github.com/aiogram/aiogram'),
 )
 
@@ -35,7 +24,7 @@ keyboard_markup_2 = types.InlineKeyboardMarkup(one_time_keyboard=True)
 text_and_data_2 = (
     ('A2 level', 'a2'),
 )
-row_btns_2=(types.InlineKeyboardButton(text, callback_data=data) for text, data in text_and_data_2)
+row_btns_2 = (types.InlineKeyboardButton(text, callback_data=data) for text, data in text_and_data_2)
 keyboard_markup_2.add(*row_btns_2)
 
 keyboard_markup_words = types.InlineKeyboardMarkup(one_time_keyboard=True)
